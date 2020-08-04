8c81ece5bb2f15f4039aeb2bdfa6875381502c67
redazures <jwu_nyc@yahoo.com>
2020-08-04 12:25:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -2,3 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :posts, only: [:index, :show]
 end
+
+#get '/users/new', to: 'users#new', as: 'register'
+
